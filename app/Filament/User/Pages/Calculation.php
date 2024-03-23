0c9b6c6e70a6474d3b16e51216615dc8b992974b
farkmu45 <farukmicros@outlook.com>
2024-03-23 10:31:21 +0700

    Initial quistionaire design

--- app/Filament/User/Pages/Calculation.php
+++ app/Filament/User/Pages/Calculation.php
@@ -2,12 +2,20 @@
 
 namespace App\Filament\User\Pages;
 
+use App\ExpertSystem\DempsterShafer;
+use App\Models\Calculation as ModelsCalculation;
+use App\Models\Questionnaire;
 use App\Models\Symptom;
+use Filament\Actions\Action;
 use Filament\Forms\Components\Radio;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\Wizard;
+use Filament\Forms\Components\Wizard\Step;
 use Filament\Forms\Form;
+use Filament\Notifications\Notification;
 use Filament\Pages\Page;
+use Filament\Support\Enums\IconPosition;
+use Illuminate\Support\Arr;
 
 class Calculation extends Page
 {
@@ -28,13 +36,13 @@ class Calculation extends Page
     {
         $symptomps = Symptom::all()->toArray();
 
-        $steps = array_map(fn ($symptom, $key) => Wizard\Step::make($key)
+        $steps = array_map(fn ($symptom, $key) => Step::make($key)
             ->label(null)
             ->schema([
-                TextInput::make('question_id')
+                TextInput::make('symptom' . $key)
                     ->default($symptom['id'])
                     ->hidden(),
-                Radio::make('answer')
+                Radio::make('answer' . $key)
                     ->required()
                     ->label($symptom['question'])
                     ->boolean()
@@ -45,8 +53,59 @@ class Calculation extends Page
                 Wizard::make(
                     $steps
                 )
-                ->submitAction(null)
+                    ->submitAction(
+                        Action::make('submit')
+                            ->label('Submit')
+                            ->action('submit')
+                            ->button()
+                    )
             ])
             ->statePath('data');
+    }
+
+    public function submit()
+    {
+        try {
+            // Convert to associative array
+            $result = [];
+            $data = $this->data;
+
+            for ($i = 0; $i < count($data) / 2; $i++) {
+                $questionKey = "symptom" . $i;
+                $answerKey = "answer" . $i;
+                array_push($result, ['symptom_id' => $data[$questionKey], 'answer' => (bool) $data[$answerKey]]);
+            }
+
+            // Filter any question with 'no' answer
+            $filteredResult = Arr::where($result, function ($value, $key) {
+                return $value['answer'];
+            });
+
+            // Get question id
+            $questionArr = [];
+            foreach ($filteredResult as $questionaire) {
+                array_push($questionArr, $questionaire['symptom_id']);
+            }
+
+            $symptoms = Symptom::whereIn('id', $questionArr)->get()->toArray();
+
+            $calculationResult = ModelsCalculation::create(['user_id' => auth()->user()->id, 'result' => 'TYPE_1']);
+            foreach ($result as $key => $item) {
+                $item['calculation_id'] = $calculationResult->id;
+                Questionnaire::create($item);
+            }
+
+            Notification::make()
+                ->title('Saved successfully')
+                ->success()
+                ->send();
+
+            $this->redirect(route('filament.user.pages.calculation'));
+        } catch (\Throwable $th) {
+            Notification::make()
+                ->title('An error occured while calculating the data')
+                ->danger()
+                ->send();
+        }
     }
 }
