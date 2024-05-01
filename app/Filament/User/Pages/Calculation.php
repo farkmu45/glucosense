edb9148957b7fd5bfffa15e25ae707b03801924f
Bayu <bayua9731@gmail.com>
2024-05-01 13:09:24 +0700

    Fixing

--- app/Filament/User/Pages/Calculation.php
+++ app/Filament/User/Pages/Calculation.php
@@ -105,26 +105,56 @@ class Calculation extends Page
             foreach ($filteredResult as $questionaire) {
                 array_push($questionArr, $questionaire['symptom_id']);
             }
-
-            $symptoms = Symptom::whereIn('id', $questionArr)->get()->toArray();
-
+            // Filter Result Data by Answer is True
+            $filteredDataAnswerTrue = array_filter($result, function ($item) {
+                return $item['answer'] === true;
+            });
+            // Collects only the 'symptom_id' of elements that pass the filter
+            $symptomIds = array_map(function ($item) {
+                return $item['symptom_id'];
+            }, $filteredDataAnswerTrue);
+            $symptoms = Symptom::whereIn('id', $symptomIds)->get()->toArray();
+            $listOfCase = [];
+            foreach ($symptoms as $key => $value) {
+                $type = [];
+                if ($value["is_diabetes_type_1"]) {
+                    $type[] = 1;
+                }
+                if ($value["is_diabetes_type_2"]) {
+                    $type[] = 2;
+                }
+                array_push($listOfCase, [
+                    "symptom_id" => $value["id"],
+                    "type" => $type,
+                    "probability" => $value["probability"]
+                ]);
+            }
+            $values=0;
+            $type="TYPE_1";
+            if (count($listOfCase) == 1) {
+                if (count($listOfCase[0]["type"]) == 2) {
+                    $type = "TYPE_1 and TYPE_2";
+                    $values = $listOfCase[0]["probability"];
+                } else {
+                    $type = "TYPE_" . $listOfCase[0]["type"][0];
+                    $values = $listOfCase[0]["probability"];
+                }
+            }
             // Calculate diabetes type based on symptoms
-
-            $calculationResult = ModelsCalculation::create(['user_id' => auth()->user()->id, 'result' => 'TYPE_1']);
+            $calculationResult = ModelsCalculation::create(['user_id' => auth()->user()->id, 'result' => $type, "value" =>$values]);
             foreach ($result as $key => $item) {
                 $item['calculation_id'] = $calculationResult->id;
                 Questionnaire::create($item);
             }
 
             Notification::make()
-                ->title('Saved successfully')
+                ->title("Saved Successfully")
                 ->success()
                 ->send();
-
             $this->redirect(route('filament.user.pages.calculation'));
         } catch (\Throwable $th) {
             Notification::make()
-                ->title('An error occured while calculating the data')
+                ->title($th)
                 ->danger()
                 ->send();
         }
